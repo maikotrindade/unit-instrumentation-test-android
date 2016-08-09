96657dc4bb7dc1f867d7bb6b37f429c997642250
Joao Oliveira <joao.oliveira@vistajet.com>
2016-08-09 15:39:08 -0300

    Test MainActivity title

--- UnitInstrumentationTests/app/src/androidTest/java/maikotrindade/com/br/unitinstrumentationtests/ui/MainActivityTest.java
+++ UnitInstrumentationTests/app/src/androidTest/java/maikotrindade/com/br/unitinstrumentationtests/ui/MainActivityTest.java
@@ -1,17 +1,19 @@
 package maikotrindade.com.br.unitinstrumentationtests.ui;
 
 import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.filters.SmallTest;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import maikotrindade.com.br.unitinstrumentationtests.R;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -20,9 +22,12 @@ import static org.junit.Assert.*;
 /**
  * Created by joao on 8/9/16.
  */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
 public class MainActivityTest {
 
     private String mLabelHelloWorld;
+    private String mAppName;
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
@@ -30,12 +35,18 @@ public class MainActivityTest {
 
     @Before
     public void setUp() throws Exception {
-        mLabelHelloWorld = "Hello World!";
+
+        mLabelHelloWorld = mActivityRule.getActivity()
+                .getResources().getString(R.string.label_hello_world);
+
+        mAppName  = mActivityRule.getActivity()
+                .getResources().getString(R.string.app_name);
     }
 
     @Test
     public void testOnCreate() throws Exception {
         onView(withId(R.id.textview_hello_world)).check(matches(isDisplayed()));
         onView(withId(R.id.textview_hello_world)).check(matches(withText(mLabelHelloWorld)));
+        assertEquals(mActivityRule.getActivity().getTitle(), mAppName);
     }
 }
