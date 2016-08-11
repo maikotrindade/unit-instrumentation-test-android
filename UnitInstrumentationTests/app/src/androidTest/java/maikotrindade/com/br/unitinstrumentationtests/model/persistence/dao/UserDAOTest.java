bd7a2ff9ddb38b5824c4a858b9b02d1875183f27
ricardo.sgobbe <Ricardo.Sgobbe@vistajet.com>
2016-08-11 09:35:27 -0300

    dao test

---
+++ UnitInstrumentationTests/app/src/androidTest/java/maikotrindade/com/br/unitinstrumentationtests/model/persistence/dao/UserDAOTest.java
@@ -0,0 +1,26 @@
+package maikotrindade.com.br.unitinstrumentationtests.model.persistence.dao;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import maikotrindade.com.br.unitinstrumentationtests.model.persistence.entity.User;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ricardo.sgobbe on 10/08/2016.
+ */
+public class UserDAOTest {
+
+    @Test
+    public void testInsert() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+        UserDAO userDAO = Mockito.mock(UserDAO.class);
+
+    }
+}
